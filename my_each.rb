a22971c54fe564a29b66743e52f784d9c42ff38b
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 21:19:24 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+class Array
+
+  def my_each(array)
+    if block_given?
+      i = 0 
+      while i < self.size
+        yield(self[i])
+        i += 1
+      end
+    end
+  end
+  
 end
